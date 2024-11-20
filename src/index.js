ab65babf4f469c19df00d83f661c6f20a117af28
Mark Johnson <mark.johnson@govx.com>
2024-11-20 14:38:38 -0800

    GovX specific UI changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /**
  * @module braintree-web-drop-in
  * @description There are two ways to integrate Drop-in into your page: a script tag integration and a JavaScript integration using [`dropin.create`](#.create).
@@ -103,15 +103,15 @@
  * </form>
  */
 
-var Dropin = require('./dropin');
-var client = require('braintree-web/client');
-var createFromScriptTag = require('./lib/create-from-script-tag');
-var constants = require('./constants');
-var analytics = require('./lib/analytics');
-var DropinError = require('./lib/dropin-error');
-var wrapPromise = require('@braintree/wrap-promise');
+var Dropin = require("./dropin-govx");
+var client = require("braintree-web/client");
+var createFromScriptTag = require("./lib/create-from-script-tag");
+var constants = require("./constants");
+var analytics = require("./lib/analytics");
+var DropinError = require("./lib/dropin-error");
+var wrapPromise = require("@braintree/wrap-promise");
 
-var VERSION = '__VERSION__';
+var VERSION = "__VERSION__";
 
 /**
  * @typedef {object} cardCreateOptions The configuration options for cards. Internally, Drop-in uses [Hosted Fields](http://braintree.github.io/braintree-web/{@pkg bt-web-version}/module-braintree-web_hosted-fields.html) to render the card form. The `overrides.fields` and `overrides.styles` allow the Hosted Fields to be customized.
@@ -566,40 +566,50 @@ var VERSION = '__VERSION__';
 
 function create(options) {
   if (!options.authorization) {
-    return Promise.reject(new DropinError('options.authorization is required.'));
+    return Promise.reject(
+      new DropinError("options.authorization is required.")
+    );
   }
 
-  return client.create({
-    authorization: options.authorization
-  }).catch(function (err) {
-    return Promise.reject(new DropinError({
-      message: 'There was an error creating Drop-in.',
-      braintreeWebError: err
-    }));
-  }).then(function (clientInstance) {
-    clientInstance = setAnalyticsIntegration(clientInstance);
+  return client
+    .create({
+      authorization: options.authorization,
+    })
+    .catch(function (err) {
+      return Promise.reject(
+        new DropinError({
+          message: "There was an error creating Drop-in.",
+          braintreeWebError: err,
+        })
+      );
+    })
+    .then(function (clientInstance) {
+      clientInstance = setAnalyticsIntegration(clientInstance);
 
-    if (clientInstance.getConfiguration().authorizationType === 'TOKENIZATION_KEY') {
-      analytics.sendEvent(clientInstance, 'started.tokenization-key');
-    } else {
-      analytics.sendEvent(clientInstance, 'started.client-token');
-    }
+      if (
+        clientInstance.getConfiguration().authorizationType ===
+        "TOKENIZATION_KEY"
+      ) {
+        analytics.sendEvent(clientInstance, "started.tokenization-key");
+      } else {
+        analytics.sendEvent(clientInstance, "started.client-token");
+      }
 
-    return new Promise(function (resolve, reject) {
-      new Dropin({
-        merchantConfiguration: options,
-        client: clientInstance
-      })._initialize(function (err, instance) {
-        if (err) {
-          reject(err);
+      return new Promise(function (resolve, reject) {
+        new Dropin({
+          merchantConfiguration: options,
+          client: clientInstance,
+        })._initialize(function (err, instance) {
+          if (err) {
+            reject(err);
 
-          return;
-        }
+            return;
+          }
 
-        resolve(instance);
+          resolve(instance);
+        });
       });
     });
-  });
 }
 
 function setAnalyticsIntegration(clientInstance) {
@@ -617,7 +627,11 @@ function setAnalyticsIntegration(clientInstance) {
 }
 
 // we check for document's existence to support server side rendering
-createFromScriptTag(create, typeof document !== 'undefined' && document.querySelector('script[data-braintree-dropin-authorization]'));
+createFromScriptTag(
+  create,
+  typeof document !== "undefined" &&
+    document.querySelector("script[data-braintree-dropin-authorization]")
+);
 
 module.exports = {
   create: wrapPromise(create),
@@ -625,5 +639,5 @@ module.exports = {
    * @description The current version of Drop-in, i.e. `{@pkg version}`.
    * @type {string}
    */
-  VERSION: VERSION
+  VERSION: VERSION,
 };
--- src/views/payment-sheet-views/index.js
+++ src/views/payment-sheet-views/index.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-var paymentOptionIDs = require('../../constants').paymentOptionIDs;
+var paymentOptionIDs = require("../../constants").paymentOptionIDs;
 
 var result = {};
 
-result[paymentOptionIDs.applePay] = require('./apple-pay-view');
-result[paymentOptionIDs.card] = require('./card-view');
-result[paymentOptionIDs.googlePay] = require('./google-pay-view');
-result[paymentOptionIDs.paypal] = require('./paypal-view');
-result[paymentOptionIDs.paypalCredit] = require('./paypal-credit-view');
-result[paymentOptionIDs.venmo] = require('./venmo-view');
+result[paymentOptionIDs.applePay] = require("./apple-pay-view");
+result[paymentOptionIDs.card] = require("./card-view-govx");
+result[paymentOptionIDs.googlePay] = require("./google-pay-view");
+result[paymentOptionIDs.paypal] = require("./paypal-view");
+result[paymentOptionIDs.paypalCredit] = require("./paypal-credit-view");
+result[paymentOptionIDs.venmo] = require("./venmo-view");
 
 module.exports = result;
